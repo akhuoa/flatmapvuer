a572156c2a9490c8201324f67591884c6965091c
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2026-08-26 09:36:13 +1200

    Update Cypress config for env variables

--- cypress.config.js
+++ cypress.config.js
@@ -14,6 +14,8 @@ const config = defineConfig({
   },
   env: {
     projectId: process.env.CYPRESS_PROJECT_ID,
+  },
+  expose: {
     visualRegressionType: 'regression',
     visualRegressionBaseDirectory: 'cypress/screenshots/MultiFlatmapVuer.cy.js/base',
     visualRegressionDiffDirectory: 'cypress/screenshots/diff',
@@ -29,20 +31,6 @@ const config = defineConfig({
   },
   component: {
     specPattern: "cypress/component/*.cy.js",
-    expose: {
-      visualRegressionType: 'regression',
-      visualRegressionBaseDirectory: 'cypress/screenshots/MultiFlatmapVuer.cy.js/base',
-      visualRegressionDiffDirectory: 'cypress/screenshots/diff',
-      visualRegressionGenerateDiff: 'always',
-      visualRegressionFailSilently: true,
-      HUMAN_FEMALE_UUID: process.env.HUMAN_FEMALE_UUID ? process.env.HUMAN_FEMALE_UUID : '',
-      HUMAN_MALE_UUID: process.env.HUMAN_MALE_UUID ? process.env.HUMAN_MALE_UUID : '',
-      RAT_UUID: process.env.RAT_UUID ? process.env.RAT_UUID : '',
-      MOUSE_UUID: process.env.MOUSE_UUID ? process.env.MOUSE_UUID : '',
-      PIG_UUID: process.env.PIG_UUID ? process.env.PIG_UUID : '',
-      CAT_UUID: process.env.CAT_UUID ? process.env.CAT_UUID : '',
-      ERROR_TOLERANCE: process.env.ERROR_TOLERANCE ? process.env.ERROR_TOLERANCE : 0.0001
-    },
     devServer: {
       framework: "vue",
       bundler: "vite",
